94b0a7289c60e8843a18f281b0ef2c4c4fac922e
William Dry <wpbdry@gmail.com>
2020-02-22 17:46:00 +0100

    create custom Questions object

--- main.js
+++ main.js
@@ -29,30 +29,43 @@
     Question.prototype.askAndCheckAnswer = function() {
         this.checkAnswer(this.ask())
     }
+
+    function Questions() {
+        this.questions = []
+    }
+
+    Questions.prototype.add = function(question, options, answerIndex) {
+        this.questions.push(new Question(question, options, answerIndex))
+    }
+
+    Questions.prototype.askRandom = function() {
+        const questionIndex = Math.floor(Math.random() * this.questions.length)
+        this.questions[questionIndex].askAndCheckAnswer()
+    }
     
-    let questions = []
+    const questions = new Questions()
     
-    questions.push(new Question(
+    questions.add(
         'What is the capital city of South Africa?',
         ['Cape Town', 'Pretoria', 'All of the above'],
         2
-    ))
-    questions.push(new Question(
+    )
+    questions.add(
         'What is the coolest university in the world?',
         ['HU', 'CODE', 'UCT', '42'],
         1
-    ))
-    questions.push(new Question(
+    )
+    questions.add(
         'How many different gender profiles are there?',
         ['None', 'One', 'Two', 'Three', 'Five', 'Eight', 'Thirteen', 'Twenty-one', 'Infinite'],
         8
-    ))
-    questions.push(new Question(
+    )
+    questions.add(
         'Is Windows a worthy operating system?',
         ['Yes', 'No'],
         1
-    ))
-    questions.push(new Question(
+    )
+    questions.add(
         `Which of the following are food?
             a) Dog
             b) Cat
@@ -61,31 +74,26 @@
         `,
         ['a and b', 'a and c', 'b and c', 'c and d', 'None of the above'],
         4
-    ))
-    questions.push(new Question(
+    )
+    questions.add(
         'What is the capital city of Germany?',
         ['Brandenburg', 'Cologne', 'Dresden', 'Berlin', 'Witzenhausen', 'Frankfurt', 'München'],
         3
-    ))
-    questions.push(new Question(
+    )
+    questions.add(
         'What is the coolest programming language?',
         ['Python', 'Pearl', 'Ruby', 'JavaScript', 'C++', 'Go', 'PHP'],
         3
-    ))
+    )
     
-    const askQuestion = function() {
-        const questionIndex = Math.floor(Math.random() * questions.length)
-        const question = questions[questionIndex]
-        question.askAndCheckAnswer()
-    }
-    
-    askQuestion()
+    questions.askRandom()
 })()
 
 
 /* IDEAS
-- askQuestion should be a method of a new Questions object
 - refactor Question.prototype.checkAnswer into multiple functions
 - display questions in the prompt, rather than in the console
 - display more human-friendly question numbers
+- create setup function to add questions
+- refactor into multiple files
 */
